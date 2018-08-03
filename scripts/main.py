6f13f487b363d3a6d97ee53f44d7379d02dbc066
krhash <krushnasharma24@gmail.com>
2018-08-03 21:53:13 +0530

    Added fuctions for populating data.

--- scripts/main.py
+++ scripts/main.py
@@ -1,19 +1,21 @@
 import json
-
 import requests
-
+import libs
 from config import API_URL, HEADERS
 
 
 def main():
-    response = requests.get(API_URL + '/cards', headers=HEADERS).json()
+    # libs.populateTopPlayers()
+    libs.populateBattleLog()
+
+    '''response = requests.get(API_URL + '/cards', headers=HEADERS).json()
     cards = [{
         'name': card['name'],
         'id': card['id'],
         'maxLevel': card['maxLevel']
     } for card in response['items']]
     with open('seeds/cards.json', 'w') as cards_file:
-        json.dump(cards, cards_file)
+        json.dump(cards, cards_file)'''
     return 0
 
 
